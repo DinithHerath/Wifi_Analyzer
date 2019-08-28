55860758bffec3bd074cf8f27e8abe381411fe66
Dinith Herath <dinithherath18332@gmail.com>
2019-08-29 04:03:01 +0530

    scan id count added.

--- __def.h
+++ __def.h
@@ -2,6 +2,7 @@ const char *ssid = "AladeenMF";
 const char *password = "12345678";
 const char *mqtt_server = "broker.hivemq.com";
 char JSONmessageBuffer[400];
+int scanCount = 0;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
